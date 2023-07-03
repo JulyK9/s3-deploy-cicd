1522f6d54bd00de1ae252d9d18e6abf34e73e8e7
JKKIM <jkesonjk@gmail.com>
2023-07-04 00:33:11 +0900

    CHORE: add ci build deploy options

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Hello Github Actions!</div>;
+  return <div className="App">Hello Github Actions CD!</div>;
 }
 
 export default App;
